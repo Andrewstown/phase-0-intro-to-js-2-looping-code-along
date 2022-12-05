4f2eceefa12b05a0318df851d02e66ee4cc71815
+ <stingeyandrewstown@live.com>
2022-12-05 12:59:03 -0600

    commit tree ee569b33bb372a22181537467daee81a42d47658

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let temp = []
+    for (let i = 0; i < names.length; i++) {
+        temp[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return temp
+}
+
+function countDown(num) {
+    let count = num
+    while (count >= 0) {
+        console.log(count)
+        count--
+    }
+}
